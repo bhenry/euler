22c81006350294bcdfda161863fc7a03432e0a37
bhenry <bhenry@campaignmonitor.com>
2023-01-06 23:08:41 -0500

    p7

--- euler/p007.py
+++ euler/p007.py
@@ -1,25 +1,13 @@
 #What is the 10 001st prime number?
 
-from math import sqrt
-
+from help import is_prime
 
 c = 0
 lp = 2
-def is_prime(n):
-    for i in range(2, sqrt(n)):
-        for j in range(2, i):
-            if i % j == 0:
-                break
-        else:
-            return True
-    return False
-
+n = 2
 while c < 10001:
-    for i in range(2, sqrt(c)):
-        for j in range(2, i):
-            if i % j == 0:
-                break
-        else:
-            c += 1
-            if c == 10001:
-                print(i)
+    if is_prime(n):
+        c += 1
+        lp = n
+    n += 1
+print(lp)
