b32433b425200df36ad9dc36cfeea93f46f74ff4
bhenry <bhenry@campaignmonitor.com>
2023-01-12 23:56:59 -0500

    51

---
+++ euler/p051.py
@@ -0,0 +1,27 @@
+from help import is_prime
+
+i = 1
+while True:
+    i += 2
+    if not is_prime(i):
+        continue
+    s = str(i)
+    for j in range(1, len(s)):
+        b = int("1" * len(s), 2)
+        for k in range(b):
+            cs = set()
+            for l in range(10):
+                bs = bin(k)[2:]
+                t = ""
+                for x,y in zip(s, bs):
+                    if y == '1':
+                        t += str(l)
+                    else:
+                        t += x
+                if t[0] == '0':
+                    continue
+                if is_prime(int(t)):
+                    cs.add(int(t))
+            if len(cs) == 8:
+                print(min(cs))
+                exit()
